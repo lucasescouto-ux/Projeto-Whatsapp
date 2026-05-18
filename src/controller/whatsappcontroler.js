9a87d9c0b9d028dd348ea471c6edd64a3420168e
lucasescouto-ux <lucas.escouto@saipos.com>
2026-05-17 21:25:32 -0300

    Aula #115.  W04  - Adicionado eventos para abrir/fechar os painéis "Editar Perfil" e "Adicionar Contato"

--- src/controller/whatsappcontroler.js
+++ src/controller/whatsappcontroler.js
@@ -7,6 +7,7 @@ class  whatsappcontroller {
 
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
 
     }
 
@@ -82,5 +83,44 @@ class  whatsappcontroller {
 
             return this.classList.contains(name);
         }
+    }
+
+    initEvents(){
+
+        this.el.myPhoto.on("click", e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+
+                this.el.panelEditProfile.addClass("open");
+            }, 300);
+        });
+
+        this.el.btnNewContact.on("click", e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+
+                this.el.panelAddContact.addClass("open");
+            }, 300);
+        });
+
+        this.el.btnClosePanelEditProfile.on("click", e=>{
+
+            this.el.panelEditProfile.removeClass("open");
+        });
+
+        this.el.btnClosePanelAddContact.on("click", e=>{
+
+            this.el.panelAddContact.removeClass("open");
+        });
+    }
+
+    closeAllLeftPanel(){
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
     }
 }
