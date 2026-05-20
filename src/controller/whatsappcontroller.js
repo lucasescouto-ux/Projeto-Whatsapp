627282d4b1830a7b3f9d52cce34d55cc83026d03
Lucas Silva <lucasescouto05@gmail.com>
2026-05-20 20:20:27 -0300

    Aula #124. W13 - Ativando a câmera com API

--- src/controller/whatsappcontroller.js
+++ src/controller/whatsappcontroller.js
@@ -197,6 +197,8 @@ class  whatsappcontroller {
             this.el.panelCamera.css({
                 "height":"calc(100% - 120px)"
             });
+
+            this._camera = new CameraController(this.el.videoCamera);
         });
 
         this.el.btnClosePanelCamera.on("click", e=>{
