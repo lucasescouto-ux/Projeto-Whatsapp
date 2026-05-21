dcf6b94650e76cc9731a4b8657da3afd967c8117
Lucas Silva <lucasescouto05@gmail.com>
2026-05-21 00:41:50 -0300

    Aula #131. W20 - Funcionalidade de leitura de capa de arquivos PDF

--- src/controller/whatsappcontroller.js
+++ src/controller/whatsappcontroller.js
@@ -253,6 +253,10 @@ export default class  whatsappcontroller {
 
             if (this.el.inputDocument.files.length) {
 
+                this.el.panelCamera.css({
+                    "height":"1%"
+                });
+
                 let file = this.el.inputDocument.files[0];
 
                 this._documentPreviewController = new DocumentPreviewController(file);
@@ -264,9 +268,15 @@ export default class  whatsappcontroller {
                     this.el.imagePanelDocumentPreview.show();
                     this.el.filePanelDocumentPreview.hide();
 
+                    this.el.panelCamera.css({
+                        "height":"calc(100% - 120px)"
+                    });
+
                 }).catch(err => {
 
-                console.log(file.type);
+                this.el.panelCamera.css({
+                    "height":"calc(100% - 120px)"
+                });
 
                 switch (file.type){
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,12 @@ const path = require('path');
 
 module.exports = {
 
-    entry: './src/app.js',
+    entry: { 
+        app: './src/app.js',
+        'pdf.worker': 'pdfjs-dist/build/pdf.worker.entry.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, '/dist'),
         publicPath: 'dist'
     }
