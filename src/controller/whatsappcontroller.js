d4aeaee608dfe33a418c8aa170876e93f488f315
Lucas Silva <lucasescouto05@gmail.com>
2026-05-21 00:03:15 -0300

    Aula #130. W19 - Funcionalidade de anexos e fotos na conversa.

--- src/controller/whatsappcontroller.js
+++ src/controller/whatsappcontroller.js
@@ -1,5 +1,6 @@
 import { format } from './../utils/format';
 import { CameraController } from './cameracontroller';
+import { DocumentPreviewController } from './documentpreviewcontroller';
 
 export default class  whatsappcontroller {
 
@@ -244,6 +245,58 @@ export default class  whatsappcontroller {
             this.el.panelDocumentPreview.css({
                 "height":"calc(100% - 120px)"
             });
+
+            this.el.inputDocument.click();
+        });
+
+        this.el.inputDocument.on("change", e => {
+
+            if (this.el.inputDocument.files.length) {
+
+                let file = this.el.inputDocument.files[0];
+
+                this._documentPreviewController = new DocumentPreviewController(file);
+
+                this._documentPreviewController.getPreviewData().then(result => {
+
+                    this.el.imgPanelDocumentPreview.src = result.src;
+                    this.el.infoPanelDocumentPreview.innerHTML = result.info;
+                    this.el.imagePanelDocumentPreview.show();
+                    this.el.filePanelDocumentPreview.hide();
+
+                }).catch(err => {
+
+                console.log(file.type);
+
+                switch (file.type){
+
+                    case 'application/vnd.ms-excel':
+                    case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+                        this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-xls';
+                    break;
+
+                    case 'application/vnd.ms-powerpoint':
+                    case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+                        this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-ppt';
+                    break;
+
+                    case 'application/msword':
+                    case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                        this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-doc';
+                    break;
+                    
+                    default:
+
+                        this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic';
+                        break;
+
+                }
+
+                    this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+                    this.el.imagePanelDocumentPreview.hide();
+                    this.el.filePanelDocumentPreview.show();
+                });
+            }
         });
 
         this.el.btnClosePanelDocumentPreview.on("click", e=>{
