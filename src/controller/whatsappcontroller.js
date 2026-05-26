4187a84919eb6caf967cff62de6838ddf6d84488
Lucas Silva <lucasescouto05@gmail.com>
2026-05-25 22:56:56 -0300

    Aula #137. W26 - Firebase authentication

--- src/controller/whatsappcontroller.js
+++ src/controller/whatsappcontroller.js
@@ -10,12 +10,30 @@ export default class  whatsappcontroller {
 
         console.log("Whats OK!");
 
-
+        this._firebase = new Firebase();
+        this.initAuth();
         this.elementsPrototype();
         this.loadElements();
         this.initEvents();
-        this._firebase = new Firebase();
 
+
+    }
+
+    initAuth(){
+
+        this._firebase.initAuth()
+        .then(response=>{
+
+            this._user = response.user;
+
+            this.el.appContent.css({
+                display: 'flex'
+            });
+        })
+        .catch(err=>{
+            
+            console.error(err);
+        });
     }
 
     loadElements(){
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,10 +1,10 @@
-const firebase = require('firebase');
-require('firebase/firestore');
+const firebase = require("firebase/app");
+require("firebase/firestore");
+require("firebase/storage");
+require("firebase/auth");
 
 export class Firebase {
-
-    constructor(){
-
+    constructor() {
         this._config = {
             apiKey: "AIzaSyD--yKs3F53VYSkI2I9oRhYCAaqWjM0F1Q",
             authDomain: "whatsapp-clone-78124.firebaseapp.com",
@@ -13,29 +13,40 @@ export class Firebase {
             messagingSenderId: "589551792845",
             appId: "1:589551792845:web:ea5dc143257f4b40e12f84",
             measurementId: "G-H5GQJCDPH6"
-
         };
 
         this.init();
     }
 
-    init(){
-
-        if(!this._initialized) {
-
+    init() {
+        if (!firebase.apps.length) {
             firebase.initializeApp(this._config);
-
-            this._initialized = true;
         }
     }
 
-    static db(){
-
+    static db() {
         return firebase.firestore();
     }
 
-    static hd(){
-
+    static hd() {
         return firebase.storage();
+    }
+
+    initAuth() {
+        return new Promise((s, f) => {
+            let provider = new firebase.auth.GoogleAuthProvider();
+
+            firebase.auth().signInWithPopup(provider)
+                .then(result => {
+                    let token = result.credential.accessToken;
+                    let user = result.user;
+
+                    s({user, 
+                        token});
+                })
+                .catch(err => {
+                    f(err);
+                });
+        });
     }
 }
