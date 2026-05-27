8d19a455d83a3aa189f2fec224c8f4d34cca369c
Lucas Silva <lucasescouto05@gmail.com>
2026-05-26 21:55:32 -0300

    Aula #141. W30 - Funcionalidade de adicionar contato

--- src/controller/whatsappcontroller.js
+++ src/controller/whatsappcontroller.js
@@ -28,36 +28,35 @@ export default class  whatsappcontroller {
 
             this._user = new User(response.user.email);
 
-            this._user.on('datachange', data =>{
+            this._user.on('datachange', data => {
 
                 document.querySelector('title').innerHTML = data.name + ' - WhatsApp Clone';
 
                 this.el.inputNamePanelEditProfile.innerHTML = data.name;
 
-                if (data.photo){
-
-                    let photo = this.el.imgPanelEditProfile;
-                    photo.src = data.photo;
-                    photo.show();
+                if (data.photo) {
+                    this.el.imgPanelEditProfile.src = data.photo;
+                    this.el.imgPanelEditProfile.show();
                     this.el.imgDefaultPanelEditProfile.hide();
 
                     let photo2 = this.el.myPhoto.querySelector('img');
                     photo2.src = data.photo;
                     photo2.show();
-                }
-            });
+                    }
+                });
 
                 this._user.name = response.user.displayName;
                 this._user.email = response.user.email;
+
+                if (response.user.photoURL) {
                 this._user.photo = response.user.photoURL;
-                
-                this._user.save().then(()=>{
+                }
 
-                    this.el.appContent.css({
-
-                        display: 'flex'
-                    });
+                this._user.save().then(() => {
+                this.el.appContent.css({
+                    display: 'flex'
                 });
+            });
         })
         .catch(err=>{
             
@@ -217,7 +216,25 @@ export default class  whatsappcontroller {
         this.el.formPanelAddContact.on("submit", e=>{
 
             e.preventDefault();
-            let ormData = new FormData(this.el.formPanelAddContact);
+            let formData = new FormData(this.el.formPanelAddContact);
+
+            let contact = new User(formData.get('email'));
+
+            contact.on('datachange', data=>{
+
+                if(data.name){
+
+                    this._user.addContact(contact).then(()=>{
+
+                        this.el.btnClosePanelAddContact.click();
+                        console.info('Contato foi adicionado.');
+                    });
+
+                } else {
+
+                    console.error('Usuário não foi encontrado.');
+                }
+            });
         });
 
         this.el.contactsMessagesList.querySelectorAll(".contact-item").forEach(item=>{
